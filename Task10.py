b1e784463b5ce3c1b96e02f84b3582622511ed92
Mayrambek-temir <m.temirov2015@gmail.com>
2019-11-17 10:07:06 +0300

    task10

---
+++ Task10.py
@@ -0,0 +1,13 @@
+string='Python language'
+
+print(string[2])
+print(string[-1])
+print(string[0:5])
+print(string[0:13])
+print(string[2::2])
+print(string[1::2])
+print(string[::-1])
+st2=string[::-1]
+print(st2[1::2])
+print(len(string))
+
